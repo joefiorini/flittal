e1fc0ac7cebdcc3e048e6cb7f71920c10ba7fc1e
Joe Fiorini <joe@joefiorini.com>
2014-11-20 10:12:41 -0500

    Start implementing drag behavior

--- board.elm
+++ board.elm
@@ -1,21 +1,26 @@
+import Mouse
+import Debug
 import Transform2D (translation)
 import Window
+import Drag
+import Time
 
 type Box =
   { x: Float
   , y: Float
   , width: Float
   , height: Float
+  , selected: Bool
   }
 
 type Board =
-  { cursorX: Float
-  , cursorY: Float
+  { cursorX: Int
+  , cursorY: Int
   , selectedBox: Maybe Box
-  , width: Int
-  , height: Int
   , boxes: [Box]
   }
+
+data Update = Move (Int, Int) | Start (Int, Int)
 
 newBox : (Float, Float) -> Box
 newBox (a,b) =
@@ -25,19 +30,36 @@ newBox (a,b) =
   , height = 50
   }
 
-defaultBoard : (Int, Int) -> Board
-defaultBoard (w,h) =
-  { width = w
-  , height = h
-  , cursorX = 0
+defaultBoard : Board
+defaultBoard =
+  { cursorX = 0
   , cursorY = 0
   , selectedBox = Nothing
   , boxes = [newBox (-100, 5), newBox (200, 30)]
   }
 
-main = lift displayBoard (defaultBoard <~ Window.dimensions)
+boardState : Signal Board
+boardState = foldp updateBoard defaultBoard dragger
 
-displayBoard {width,height,cursorX,cursorY,selectedBox,boxes} = collage width height <| map boxToRect boxes
+findFirst : (a -> Bool) -> [a] -> Maybe a
+findFirst p l = let filtered = filter p l in
+  if length filtered == 1 then Just head filtered else Nothing
+
+selectBox : Board -> (Int, Int) -> Maybe Box
+selectBox board (x,y) = findFirst (\box -> x > box.x && x < box.width && y > box.y && y < box.height) board.boxes
+
+
+dragger = merge Drag.start <| Drag.lastPosition (40 * Time.millisecond)
+main = lift2 displayBoard Window.dimensions boardState
+
+updateBoard locationM board = Debug.watch "board" <| case locationM of
+  Move box -> { board | selectedBox <- box }
+  Start (x,y) -> { board | cursorX <- x, cursorY <- y }
+  _ -> board
+
+displayBoard : (Int, Int) -> Board -> Element
+displayBoard (w,h) {cursorX,cursorY,selectedBox,boxes} =
+  collage w h <| map boxToRect boxes
 
 placeBox : (Float, Float) -> Form -> Form
 placeBox (x,y) form = groupTransform (translation x y) [form]
