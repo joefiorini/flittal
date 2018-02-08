65d299476e451d6782227b566b6d80f63cdd1ea6
Joe Fiorini <joe@joefiorini.com>
2018-02-07 21:18:49 -0500

    Update release notes

--- src/Partials/Releases.elm
+++ src/Partials/Releases.elm
@@ -1,15 +1,23 @@
-module Partials.Releases where
+module Partials.Releases exposing (..)
 
 import Markdown
 
+
 view =
-  Markdown.toHtml """
+    Markdown.toHtml """
 
 ## What's New
 
+### [1.1.1](https://github.com/joefiorini/flittal/releases/1.1.0)
+
+Third release in just as many years, codenamed "Better Late Than Never". This release fixes the long broken Share feature. You can now use the "Share this Board" field above the board to get a link to share or come back to your board. Two important notes:
+
+  1. Your data is **NOT** sent to any servers; all your data is in the share URL
+  2. If you leave the site or refresh the page, you will lose your board, so be careful!
+
 ### [1.1.0](https://github.com/joefiorini/flittal/releases/1.1.0)
 
-Second release brings Undo/Redo functionality. Press `u` to undo an action and `ctrl+r` to redo it. I also fixed some bugs related to deleting multiple selections at once, and a bug around disconnecting boxes. I also added this \"What's New\" sidebar so you can keep track of recent changes.
+Second release brings Undo/Redo functionality. Press `u` to undo an action and `ctrl+r` to redo it. I also fixed some bugs related to deleting multiple selections at once, and a bug around disconnecting boxes. I also added this "What's New" sidebar so you can keep track of recent changes.
 
 #### Coming Soon
 
